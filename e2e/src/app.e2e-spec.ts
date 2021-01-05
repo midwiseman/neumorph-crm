8fd1ebd311ad49ca5f1064feceba884fcc2b3788
midwiseman <michael@myagentally.com>
2021-01-05 10:33:53 -0500

    renaming project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('neumorph-crm app is running!');
+    expect(page.getTitleText()).toEqual('rude-crm app is running!');
   });
 
   afterEach(async () => {
